656488a17bf7fd509b356f657312d24f8684b5e1
Darshan Dorai <darsh.d2@gmail.com>
2017-01-14 14:43:25 +0530

    Initial commit

---
+++ app/src/main/java/com/udacity/stockhawk/ui/fragment/StockListFragment.java
@@ -0,0 +1,14 @@
+package com.udacity.stockhawk.ui.fragment;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+
+/**
+ * Created by darshan on 14/1/17.
+ */
+
+public class StockListFragment implements SwipeRefreshLayout.OnRefreshListener {
+    @Override
+    public void onRefresh() {
+
+    }
+}
