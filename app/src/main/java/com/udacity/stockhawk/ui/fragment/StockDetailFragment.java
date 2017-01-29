073012445919fe080a6f2a3ddef2e9b89a2a1ae6
Darshan Dorai <darsh.d2@gmail.com>
2017-01-29 13:18:09 +0530

    Retrieve historical stock quote data
    Using RxAndroid 2 and RxJava 2, asynchronously retrieve historical
    stock data from DB and parse it to a format that can be used for
    populating the graph.

--- app/src/main/java/com/udacity/stockhawk/ui/fragment/StockDetailFragment.java
+++ app/src/main/java/com/udacity/stockhawk/ui/fragment/StockDetailFragment.java
@@ -1,6 +1,7 @@
 package com.udacity.stockhawk.ui.fragment;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,11 +11,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.data.Entry;
 import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.data.Contract;
 import com.udacity.stockhawk.util.Constants;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 import timber.log.Timber;
 
 /**
@@ -66,12 +77,73 @@ public class StockDetailFragment extends Fragment {
         textViewStockName.setPadding(toolbar.getTitleMarginStart(), 0, 0, 0);
         textViewStockName.setText(stockName);
 
+        loadHistoricalStockQuotes();
+
         return view;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        disposableSingleObserver.dispose();
+        if (disposableSingleObserver.isDisposed()) {
+            Timber.d("isDisposed");
+        }
         Timber.d("onDestroyView");
+    }
+
+    private DisposableSingleObserver<ArrayList<Entry>> disposableSingleObserver;
+
+    private void loadHistoricalStockQuotes() {
+        Single<ArrayList<Entry>> stockQuotesSingle = Single.fromCallable(new Callable<ArrayList<Entry>>() {
+            @Override
+            public ArrayList<Entry> call() throws Exception {
+                return retrieveStockQuotesFromDB();
+            }
+        });
+        stockQuotesSingle
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread());
+        disposableSingleObserver = stockQuotesSingle
+                .subscribeWith(new DisposableSingleObserver<ArrayList<Entry>>() {
+                    @Override
+                    public void onSuccess(ArrayList<Entry> value) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+                });
+    }
+
+    private ArrayList<Entry> retrieveStockQuotesFromDB() {
+        Cursor cursor = getContext().getContentResolver()
+                .query(
+                        Contract.Quote.makeUriForStock(stockSymbol),
+                        new String[]{ Contract.Quote.COLUMN_HISTORY },
+                        null,
+                        null,
+                        null
+                );
+        if (cursor == null) {
+            throw new NullPointerException();
+        }
+        String history = "";
+        if (cursor.moveToNext()) {
+            history = cursor.getString(cursor.getColumnIndex(Contract.Quote.COLUMN_HISTORY));
+        }
+        cursor.close();
+
+        String[] historicalQuotes = history.split("\n");
+        int numHistoricalQuotes = historicalQuotes.length;
+        ArrayList<Entry> stockQuotes = new ArrayList<>(numHistoricalQuotes);
+        for (int i = 0, l = historicalQuotes.length; i < l; i++) {
+            String[] entry = historicalQuotes[i].split(", ");
+            stockQuotes.add(new Entry(Float.parseFloat(entry[0]), (new BigDecimal(entry[1])).floatValue()));
+            Timber.d(stockQuotes.get(i).getX() + ", " + stockQuotes.get(i).getY());
+        }
+        return stockQuotes;
     }
 }
